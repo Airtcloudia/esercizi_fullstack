1515f3c45d44cf07b250b3cb74c28da14e4129a3
Airtcloudia <klau26@hotmail.it>
2023-08-30 12:20:41 +0200

    add exercise 28 javascript

--- exercise_28_javascript/exercise.js
+++ exercise_28_javascript/exercise.js
@@ -1,6 +1,13 @@
-function sum(num1, num2, num3) {
+/* function sum(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(numbers[0], numbers[1], numbers[2])); */
+
+function sum(...number) {
+  return number.reduce((acc,val) => acc +val, 0);
+}
+
+const numbers = [1, 2, 3];
+console.log(sum(numbers[0], numbers[1], numbers[2])); 
