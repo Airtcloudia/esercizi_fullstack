bf849a51685178c04908a596bd86e94c2fd45ea1
Airtcloudia <klau26@hotmail.it>
2023-09-01 10:13:40 +0200

    add exercise 30 javascript

--- exercise_30_javascript/exercise.js
+++ exercise_30_javascript/exercise.js
@@ -1,3 +1,20 @@
+/*class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+}
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer); */
+
 class Person {
   constructor(id, firstName, lastName, age) {
     this.id = id;
@@ -8,6 +25,11 @@ class Person {
 
   toJson() {
     return JSON.stringify(this);
+  }
+
+static fromJson(json) { //static fromJson che va direttamente alla classe json
+    const data = JSON.parse(json);
+    return new Person(data.id, data.firstName, data.lastName, data.age);
   }
 }
 
