92e4448da29a50e475be388bfed17bf197ac2703
Airtcloudia <klau26@hotmail.it>
2023-09-13 12:13:08 +0200

    add exercise 35 javascript

--- exercise_35_javascript/exercise.js
+++ exercise_35_javascript/exercise.js
@@ -1,1 +1,13 @@
 
+function repeatHello(callback){
+    setInterval(callback, 1000);
+}
+
+
+const callbackfunc = () => {
+    console.log("Hello")
+}
+
+repeatHello (callbackfunc);
+
+
--- exercise_36_javascript/exercise.js
+++ exercise_36_javascript/exercise.js
@@ -1,1 +1,12 @@
 
+
+function repeatHello(callback){
+    setTimeout(callback, 1000);
+}
+
+// setTimeout(function() {
+//     console.log(callback);
+//   }, 1000);
+// } 
+
+repeatHello (() => console.log("Hello"));
