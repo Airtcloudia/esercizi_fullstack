1357c485b554cd917cfd9903c76468e31c35557a
Airtcloudia <klau26@hotmail.it>
2023-09-25 10:24:54 +0200

    add exercise 41 javascript

---
+++ exercise_41_javascript/exercise.js
@@ -0,0 +1,41 @@
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+
+const saveLocalStorage = () => {
+  try {
+
+    const saveUser = JSON.stringify(user);
+    localStorage.setItem("user", saveUser);
+
+  }
+
+  catch (error) {
+    outError(error)
+  }
+
+}
+
+
+const getLocalStorage = () => {
+  try {
+    const getUser = localStorage.getItem("user");
+    const userString = JSON.parse(getUser);
+    console.log(userString);
+
+  } catch (error) {
+    outError(error)
+
+  }
+
+}
+
+
+const init = () => {
+  saveLocalStorage();
+  getLocalStorage();
+}
+
